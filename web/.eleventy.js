3d019d3b527c0c341c1121e8d4b436b3313412e2
Federico Monfasani <fmonfasani@gmail.com>
2025-09-15 23:07:46 -0300

    fix: pathPrefix + módulos + UI mínima

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -1,4 +1,7 @@
 module.exports = (cfg) => {
   cfg.addPassthroughCopy("src/assets");
-  return { dir: { input:"src", output:"dist", includes:"_includes", data:"_data" } };
+  return {
+    dir: { input:"web/src", output:"web/dist", includes:"_includes", data:"_data" },
+    pathPrefix: "/MVPSaaS/",
+  };
 };
